29386ada5fcf2bf9c6c6885bbd7e1cf5570332a5
Junaid Azhar Shaikh <junaidazharshaikh@gmail.com>
2019-08-24 13:13:52 -0400

    added router

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="ui secondary pointing menu">
-        <a href="/" class="active item">Save Me Some Space</a>
+        <a href="/" class="active item">
+            Save Me Some Space
+        </a>
+        <div class="right menu">
+            <a href="#" class="ui item" @click="login">
+                Login
+            </a>
+        </div>
     </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+    methods: mapActions(['login'])
+
 }
 </script>
 
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,16 +1,31 @@
+import api from '@/api/imgur'
+
 const state = {
     token: null
 }
 
 const getters = {
     isLoggedIn : state =>  !!state.token 
-    
 }
 
 const mutations = {
-
+    setToken: (state, token) => {
+        state.token = token
+    }
 }
 
 const actions = {
+    logout: ({ commit }) => {
+        commit('setToken', null)
+    },
+    login:() => {
+        api.login();
+    }
+}
 
+export default {
+    state,
+    getters,
+    actions,
+    mutations
 }

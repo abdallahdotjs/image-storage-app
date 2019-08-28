f869a57c542d7739b25a77b72aba03d1386e8458
Junaid Azhar Shaikh <junaidazharshaikh@gmail.com>
2019-08-27 22:55:52 -0400

    adding unit testing to project

---
+++ src/test/unit/app-header.spec.js
@@ -0,0 +1,9 @@
+import { mount} from 'vue/test-utils'
+import AppHeader from '@/components/AppHeader'
+
+describe('Header.spec.js', () => {
+    it('should render component', () => {
+        const wrapper = mount(AppHeader)
+        
+    })
+})
